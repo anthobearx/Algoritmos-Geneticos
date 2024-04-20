154a23060f98a4ac1d48988e4d726d67bd785ff9
anthobearx <anthobearmc@gmail.com>
2024-04-20 16:16:06 -0700

    primera version :)

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    /**
+     *
+          private final double[] inversiones = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+          private final double[] beneficioI = {0, 0.28, 0.45, 0.65, 0.78, 0.90, 1.02, 1.13, 1.23, 1.32, 1.38};
+          private final double[] beneficioII = {0, 0.25, 0.41, 0.55, 0.65, 0.75, 0.8, 0.85, 0.88, 0.9, 0.9};
+          private final double[] beneficioIII = {0, 0.15, 0.25, 0.40, 0.50, 0.62, 0.73, 0.82, 0.90, 0.96, 1.00};
+          private final double[] beneficioIV = {0, 0.2, 0.33, 0.42, 0.48, 0.53, 0.56, 0.58, 0.6, 0.6, 0.6};
+     *
+     * **/
+}
